d9dbb333d3b7d03bdff3572c57e92f3c70a7de9e
Raymond <rayxrealms@gmail.com>
2023-04-10 15:20:34 -0400

    Create post functionality (stays on page refresh or navigate)

--- hifi/index.js
+++ hifi/index.js
@@ -70,7 +70,9 @@ let posts =
     </p>\
   </div>\
 </li>';
-document.getElementById("search-results").innerHTML = posts;
+localStorage.getItem("posts") && document.getElementById("search-results")
+  ? (document.getElementById("search-results").innerHTML = localStorage.getItem("posts"))
+  : false;
 // SEARCH FUNCTIONALITY
 
 const searchGroup = () => {
@@ -140,4 +142,25 @@ const searchCommunities = () => {
   }
 };
 
-//
+// CREATE POST
+if (document.getElementById("createpost-container")) {
+  let createpostForm = document.getElementById("createpost-container");
+  createpostForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let username = "Sally Pear";
+    let groupname = document.getElementById("createpost-groupname").value;
+    let classname = document.getElementById("createpost-class").value;
+    let body = document.getElementById("createpost-body").value;
+    if (groupname === "" || classname === "") {
+      alert("Please enter a group name and class");
+      return;
+    } else {
+      newPost = document.createElement("li");
+      newPost.innerHTML = `<div class="post"><p class="post-meta"><span>${classname} </span>&#x2022 posted by ${username}</p>\
+  <h4>${groupname}</h4><p>${body}</p></div>`;
+      let newPosts = newPost.innerHTML + posts;
+      localStorage.setItem("posts", newPosts);
+      window.location.href = "search.html";
+    }
+  });
+}
