9bca1f6635ce8e4841815b37aa783dc7ee57a071
chasewil <chasewil@umich.edu>
2023-04-11 16:51:03 -0400

    styling changes

--- hifi/login.js
+++ hifi/login.js
@@ -1,11 +1,11 @@
 function checkLogin() {
     let username = document.getElementsByClassName("inputField")[0].value;
     let password = document.getElementsByClassName("input2field")[0].value;
-    if(username != "spears@umich.edu"){
-        alert("Username is incorrect, please try again")
+    if(username != "pears@umich.edu"){
+        alert("Username or password is incorrect, please try again")
     }
     else if(password != "password"){
-        alert("Password is incorrect, please try again")
+        alert("Username or password is incorrect, please try again")
     }
     else{
         window.location.href = "index.html";
